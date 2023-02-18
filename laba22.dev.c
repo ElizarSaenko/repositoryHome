04ab1cd814d1cbcb89971530ddaee86fe60a307d
ElizarSaenko <elizarsaenko@gmail.com>
2023-02-18 13:22:30 +1000

    Create laba22.dev.c

---
+++ laba22.dev.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void main()
+   {
+  int arr1[10][10],i,j,n;
+  int det=0;
+  
+  
+       printf("\n\nCalculate the determinant of a 3 x 3 matrix :\n");
+       printf("-------------------------------------------------\n");  
+	 printf("Input elements in the first matrix :\n");
+       for(i=0;i<3;i++)
+        {
+            for(j=0;j<3;j++)
+            {
+	           printf("element - [%d],[%d] : ",i,j);
+	           scanf("%d",&arr1[i][j]);
+            }
+    printf("\nAaaaaiiay iao?eoa:\n"); 
+    for (i = 0, i < n; i++) 
+    { 
+        for (int j = 0; j < b; j++) 
+        { 
+            printf("%f  ", x[i][j]); 
+        } 
+        printf("\n");
+        } 
+}
