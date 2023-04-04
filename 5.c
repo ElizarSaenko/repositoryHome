80cb2c77a630e8b9ec198afd833fa2720d2d39b3
ElizarSaenko <elizarsaenko@gmail.com>
2023-04-04 23:43:54 +1000

    Update 5.c

--- 5.c
+++ 5.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// объ€вление функции, котора€ будет выполн€ть операцию с матрицами
+/// объ€вление функции, котора€ будет выполн€ть операцию с матрицами
 double* performOperation(double* matrix1, double* matrix2, int n, char operation);
 
-int main() {
+int main() 
+{
     int n, i, j;
     char operation;
     double* matrix1;
@@ -12,7 +13,7 @@ int main() {
     double* result;
 
     // запрос размерности матриц
-    printf("Enter the dimension of the matrices (N): ");
+    printf("Enter the size of matrices (N): ");
     scanf("%d", &n);
 
     // динамическое выделение пам€ти
@@ -21,16 +22,21 @@ int main() {
 
     // запрос элементов первой матрицы
     printf("Enter the elements of the first matrix: \n");
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
+    for (i = 0; i < n; i++)
+    matrix1 = (double*)malloc(n * n * sizeof(double));
+	{
+        for (j = 0; j < n; j++) 
+		{
             scanf("%lf", &matrix1[i * n + j]);
         }
     }
 
     // запрос элементов второй матрицы
     printf("Enter the elements of the second matrix: \n");
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
+    for (i = 0; i < n; i++) 
+	{
+        for (j = 0; j < n; j++) 
+		{
             scanf("%lf", &matrix2[i * n + j]);
         }
     }
@@ -44,8 +50,10 @@ int main() {
 
     // вывод результата
     printf("Resulting Matrix:\n");
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
+    for (i = 0; i < n; i++) 
+	{
+        for (j = 0; j < n; j++)
+		{
             printf("%lf ", result[i * n + j]);
         }
         printf("\n");
@@ -68,27 +76,35 @@ double* performOperation(double* matrix1, double* matrix2, int n, char operation
     result = (double*)malloc(n * n * sizeof(double));
 
     // проверка операции и вычисление результата
-    switch (operation) {
+    switch (operation) 
+	{
         case '+':
-            for (i = 0; i < n; i++) {
-                for (j = 0; j < n; j++) {
+            for (i = 0; i < n; i++) 
+			{
+                for (j = 0; j < n; j++) 
+				{
                     result[i * n + j] = matrix1[i * n + j] + matrix2[i * n + j];
                 }
             }
             break;
         case '-':
-            for (i = 0; i < n; i++) {
-                for (j = 0; j < n; j++) {
+            for (i = 0; i < n; i++) 
+			{
+                for (j = 0; j < n; j++) 
+				{
                     result[i * n + j] = matrix1[i * n + j] - matrix2[i * n + j];
                 }
             }
             break;
         case '*':
-            for (i = 0; i < n; i++) {
-                for (j = 0; j < n; j++) {
+            for (i = 0; i < n; i++) 
+			{
+                for (j = 0; j < n; j++) 
+				{
                 	int k;
                     result[i * n + j] = 0;
-                    for (k = 0; k < n; k++) {
+                    for (k = 0; k < n; k++) 
+					{
                         result[i * n + j] += matrix1[i * n + k] * matrix2[k * n + j];
                     }
                 }
